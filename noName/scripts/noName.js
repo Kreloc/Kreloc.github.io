fb0e35ded913e98844e0e068536b82ba0d1f7645
Kreloc <killianDeDemon@yahoo.com>
2019-03-19 19:06:25 -0800

    Adding dice to use for percentage determinations.

--- noName/scripts/noName.js
+++ noName/scripts/noName.js
@@ -19,6 +19,17 @@ var currentArea = [];
 Array.prototype.randomElement  = function(){
     return this[Math.floor(Math.random() * this.length)];
   }
+
+// roll dice to determine percentage
+function dynamicallyLoadScript(url) {
+    var script = document.createElement("script");  // create a script DOM node
+    script.src = url;  // set its src to the provided URL
+
+    document.head.appendChild(script);  // add it to the end of the head section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
+}
+dynamicallyLoadScript("/scripts/dice.js");
+var dice = new Dice(); // defaults to +new Date() as a seed, can use any number of arguments for seeding
+
 // show loading text???
 document.getElementById("areaDescription").innerHTML = "Loading...";
 // const request = async () => {
